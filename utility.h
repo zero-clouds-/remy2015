8b11b75e774d3af4d190b489f481066e642b8d26
aephynsa <aephynsa@gmail.com>
2015-11-19 14:55:13 -0500

    Utility things.

--- utility.h
+++ utility.h
@@ -32,16 +32,24 @@ void delete_buffer(buffer* b) {
 void clear_buffer(buffer* b) {
   b->len = 0;
 }
+void resize_buffer(buffer* b, int size) {
+  if (size < b->len) b->len = size;
+  unsigned char* temp = (unsigned char*)malloc(size * sizeof(unsigned char));
+  memcpy(temp, b->data, b->len * sizeof(unsigned char));
+  free(b->data);
+  b->data - temp;
+  b->size = size;
+}
 void append_buffer(buffer* b, unsigned char const* str, int len) {
-  if (b->len + len > b->size) {
-    unsigned char* temp = (unsigned char*)malloc(b->size * 2 * sizeof(unsigned char));
-    memcpy(temp, b->data, b->len * sizeof(unsigned char));
-    free(b->data);
-    b->data = temp;
-    b->size *= 2;
-  }
+  if (b->len + len > b->size) resize_buffer(b, b->size * 2);
   memcpy(b->data + b->len, str, len * sizeof(unsigned char));
   b->len += len;
+}
+void write_buffer(buffer* b, char const* filename) {
+  FILE* fp = fopen(filename, "wb");
+  if (!fp) error("fopen()");
+  fwrite(b->data, sizeof(unsigned char), b->len, fp);
+  fclose(fp);
 }
 
 #endif
