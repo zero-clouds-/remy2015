2347d217e185d457e64fdaa2da8224024d4a7616
zero-clouds- <s.edwin.green@gmail.com>
2015-11-25 17:01:20 -0500

    updated header size and indexes to match class spec

--- protocol/udp_protocol.h
+++ protocol/udp_protocol.h
@@ -10,16 +10,17 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-#define UP_HEADER_LEN 24
-#define UP_IDENTIFIER 0
-#define UP_CLIENT_REQUEST 1
-#define UP_REQUEST_DATA 2
-#define UP_BYTE_OFFSET 3
-#define UP_TOTAL_SIZE 4
-#define UP_PAYLOAD_SIZE 5
+#define UP_HEADER_LEN 28
+#define UP_VERSION 0
+#define UP_IDENTIFIER 1
+#define UP_CLIENT_REQUEST 2
+#define UP_REQUEST_DATA 3
+#define UP_BYTE_OFFSET 4
+#define UP_TOTAL_SIZE 5
+#define UP_PAYLOAD_SIZE 6
 
 typedef struct header_t {
-  uint32_t data[5];
+  uint32_t data[7];
 } header; 
 
 void insert_header(buffer* datagram, header h);
